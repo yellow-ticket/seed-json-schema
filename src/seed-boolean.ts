34c10263ef94cb0e0c84e4991cb59d973da68c3a
Artem Zakharchenko <kettanaito@gmail.com>
2024-06-12 18:17:14 +0200

    test: add seed-boolean tests

--- src/seed-boolean.ts
+++ src/seed-boolean.ts
@@ -1,5 +1,14 @@
+import type { JSONSchema7 } from 'json-schema'
 import { faker } from '@faker-js/faker'
 
-export function seedBoolean(): boolean {
+export function seedBoolean(schema: JSONSchema7): boolean {
+  if (schema.const) {
+    return schema.const as boolean
+  }
+
+  if (schema.examples) {
+    return schema.examples as boolean
+  }
+
   return faker.datatype.boolean()
 }
---
+++ tests/seed-boolean.test.ts
@@ -0,0 +1,18 @@
+import { JSONSchema7 } from 'json-schema'
+import { seedBoolean } from '../src/seed-boolean.js'
+
+it.each<[string, JSONSchema7, boolean]>([
+  ['returns a random boolean by default', { type: 'boolean' }, true],
+  [
+    'returns the boolean specified in "const"',
+    { type: 'boolean', const: true },
+    true,
+  ],
+  [
+    'returns the boolean specified in "examples"',
+    { type: 'boolean', examples: true },
+    true,
+  ],
+])('%s', (_, input, output) => {
+  expect(seedBoolean(input)).toBe(output)
+})
