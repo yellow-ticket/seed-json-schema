e1e01ece0c93d4dfee8503b654ed9c9340a3dec0
Artem Zakharchenko <kettanaito@gmail.com>
2024-06-19 13:23:22 +0200

    test(object): fix incorrect array schema

---
+++ tests/seed-object.test.ts
@@ -0,0 +1,52 @@
+import { JSONSchema7 } from 'json-schema'
+import { seedObject } from '../src/seed-object.js'
+
+it.each<[string, JSONSchema7, object]>([
+  ['returns an empty object by default', { type: 'object' }, {}],
+  [
+    'returns the "default" object if specified',
+    { type: 'object', default: { foo: 'bar' } },
+    { foo: 'bar' },
+  ],
+  [
+    'returns the "examples" object if specified',
+    { type: 'object', examples: { foo: 'bar' } },
+    { foo: 'bar' },
+  ],
+  [
+    'respects literal property types',
+    {
+      type: 'object',
+      properties: {
+        string: { type: 'string' },
+        number: { type: 'number' },
+        integer: { type: 'integer' },
+        boolean: { type: 'boolean' },
+      },
+    },
+    {
+      string: 'fully',
+      number: 100,
+      integer: 14.67,
+      boolean: true,
+    },
+  ],
+  [
+    'respects array property type',
+    {
+      type: 'object',
+      properties: {
+        friends: {
+          type: 'array',
+          items: { type: 'string' },
+          maxItems: 2,
+        },
+      },
+    },
+    {
+      friends: ['fully', 'until'],
+    },
+  ],
+])('%s', (_, input, output) => {
+  expect(seedObject(input)).toEqual(output)
+})
