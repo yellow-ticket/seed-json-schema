d4cf4ab3cccaac466a9b1a8740652991a88cd392
Artem Zakharchenko <kettanaito@gmail.com>
2024-06-28 11:35:26 +0200

    chore: fix unreliable dates

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,9 +1,6 @@
 import { faker } from '@faker-js/faker'
 
-beforeAll(() => {
-  faker.setDefaultRefDate('2024-06-08T01:02:03.000Z')
-})
-
 beforeEach(() => {
   faker.seed(1)
+  faker.setDefaultRefDate('2024-06-08T01:02:03.000Z')
 })
