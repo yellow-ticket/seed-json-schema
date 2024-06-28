fadba22a28a52af3b021ab9f0aff2936c59c19b6
Artem Zakharchenko <kettanaito@gmail.com>
2024-06-28 13:18:44 +0200

    fix: export "faker" to allow seeding values

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ export { seedInteger } from './seed-integer.js'
 export { seedNumber } from './seed-number.js'
 export { seedObject } from './seed-object.js'
 export { seedString } from './seed-string.js'
+
+export { faker } from '@faker-js/faker'
