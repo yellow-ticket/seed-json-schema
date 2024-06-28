330c28d1d0b8c28f658860f0a1676eef4a4d36cf
Artem Zakharchenko <kettanaito@gmail.com>
2024-06-28 11:51:52 +0200

    fix: add entrypoint

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { seedSchema } from './seed-schema.js'
+export { seedArray } from './seed-array.js'
+export { seedBoolean } from './seed-boolean.js'
+export { seedInteger } from './seed-integer.js'
+export { seedNumber } from './seed-number.js'
+export { seedObject } from './seed-object.js'
+export { seedString } from './seed-string.js'
