b5ec6e51c5bc857c00ab2730c5f8a77705aea4ed
Artem Zakharchenko <kettanaito@gmail.com>
2024-06-29 12:10:51 +0200

    fix: reverse "integer" and "number" values

--- tests/seed-integer.test.ts
+++ tests/seed-integer.test.ts
@@ -2,16 +2,26 @@ import { JSONSchema7 } from 'json-schema'
 import { seedInteger } from '../src/seed-integer.js'
 
 it.each<[string, JSONSchema7, number | RegExp]>([
-  ['returns a random integer by default', { type: 'integer' }, 41.7],
+  ['returns a random integer by default', { type: 'integer' }, 42],
   [
     'returns a random integer with "minimum" set',
     { type: 'integer', minimum: 100 },
-    141.7,
+    142,
   ],
   [
     'returns a random integer with "maximum" set',
     { type: 'integer', maximum: 30 },
-    12.51,
+    12,
+  ],
+  [
+    'respect "exclusiveMinimum"',
+    { type: 'integer', exclusiveMinimum: 0, maximum: 2 },
+    1,
+  ],
+  [
+    'respect "exclusiveMaximum"',
+    { type: 'integer', minimum: 1, exclusiveMaximum: 2 },
+    1,
   ],
   [
     'returns the integer specified in "const"',
--- tests/seed-number.test.ts
+++ tests/seed-number.test.ts
@@ -2,16 +2,31 @@ import { JSONSchema7 } from 'json-schema'
 import { seedNumber } from '../src/seed-number.js'
 
 it.each<[string, JSONSchema7, number | RegExp]>([
-  ['returns random number by default', { type: 'number' }, 42],
+  ['returns random number by default', { type: 'number' }, 41.7],
   [
     'returns a random number with "minimum" set',
     { type: 'number', minimum: 100 },
-    142,
+    141.7,
   ],
   [
     'returns a random number with "maximum" set',
     { type: 'number', maximum: 30 },
-    12,
+    12.51,
+  ],
+  [
+    'respects "exclusiveMinimum"',
+    { type: 'number', exclusiveMinimum: 0, maximum: 2 },
+    1.42,
+  ],
+  [
+    'respects "exclusiveMaximum"',
+    { type: 'number', minimum: 1, exclusiveMaximum: 2 },
+    1,
+  ],
+  [
+    'respects "multipleOf"',
+    { type: 'number', multipleOf: 0.5, maximum: 1 },
+    0.5,
   ],
   [
     'returns the number specified in "const"',
